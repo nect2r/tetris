fd48dda02dfd2125bce349ef9719b1d36fb6d23c
nect2r <71915361+nect2r@users.noreply.github.com>
2023-04-07 17:28:23 +0900

    room분리 생성 테스트중

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const { createClient } = require("redis");
-const { createAdapter } = require("@socket.io/redis-adapter");
+// const { createClient } = require("redis");
+// const { createAdapter } = require("@socket.io/redis-adapter");
 
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
-const pubClient = createClient({ url: "redis://localhost:6379" });
-const subClient = pubClient.duplicate();
+// const pubClient = createClient({ url: "redis://localhost:6379" });
+// const subClient = pubClient.duplicate();
 
 // http server를 socket.io server로 upgrade한다
 var io = require('socket.io')(server);
-io.adapter(createAdapter(pubClient, subClient));
+// io.adapter(createAdapter(pubClient, subClient));
 
 var lobby = io.of('/lobby');
+var room = io.of('/room');
 
 // localhost:3000으로 서버에 접속하면 클라이언트로 index.html을 전송한다
 app.get('/', function (req, res) {
@@ -22,18 +23,61 @@ app.get('/', function (req, res) {
 // 정적파일 접근허용
 app.use('/', express.static(__dirname + '/'));
 
+room.on('connection', function (socket) {
+    console.log("룸");
+    socket.join("room1");
+
+    socket.on('lobbyUsers', function (data) {
+        var sockets = room.sockets;
+        var names = new Array();
+
+        for (var socketItem of sockets) {  
+            names.push(socketItem[1].name);
+        }
+
+        room.to("room1").to(socket.id).emit('lobbyUsersRes', names);
+    });
+
+    // 접속한 클라이언트의 정보가 수신되면
+    socket.on('login', function (data) {
+        console.log('Client logged-in:\n name:' + data.name + '\n userid: ' + data.userid);
+
+        // socket에 클라이언트 정보를 저장한다
+        socket.name = data.name;
+        socket.userid = data.userid;
+
+        // 접속된 모든 클라이언트에게 메시지를 전송한다
+        room.to("room1").emit('login', data.name);
+    });
+
+    // 클라이언트로부터의 메시지가 수신되면
+    socket.on('chat', function (data) {
+        console.log('Message from %s: %s', socket.name, data.msg);
+
+        var msg = {
+            from: {
+                name: socket.name,
+                userid: socket.userid
+            },
+            msg: data.msg
+        };
+
+        // 접속된 모든 클라이언트에게 메시지를 전송한다
+        room.to("room1").emit('chat', msg);
+    });
+});
+
 // connection이 수립되면 event handler function의 인자로 socket인 들어온다
 lobby.on('connection', function (socket) {
 
-    socket.join("room1");
-    publicRooms();
+    console.log("로비");
 
     function publicRooms() {
         const {
             adapter : {
                     sids, rooms           
             } 
-        } = lobby;
+        } = room;
 
         // public room list 만들기
         const publicRooms = [];
@@ -43,10 +87,6 @@ lobby.on('connection', function (socket) {
             }
         });
 
-        publicRooms.forEach(function(room) {
-            console.log(room);
-        });
-
         return publicRooms;
     }
 
@@ -54,12 +94,11 @@ lobby.on('connection', function (socket) {
         var sockets = lobby.sockets;
         var names = new Array();
 
-        for (var socket of sockets) {  
-            names.push(socket[1].name);
+        for (var socketItem of sockets) {  
+            names.push(socketItem[1].name);
         }
 
-        lobby.to(socket.id).emit('lobbyUsersRes', names);
-        console.log(process.pid);
+        lobby.to(socket.id).emit('lobbyUsersRes', names, publicRooms());
     });
 
     // 접속한 클라이언트의 정보가 수신되면
